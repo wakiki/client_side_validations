30be35b669c6b6f0235d367e190d4d7deb8e550a
Steve Leung <scyl88@gmail.com>
2012-07-10 01:29:00 +0100

    show in lie error inline before inline-hints

--- vendor/assets/javascripts/rails.validations.js
+++ vendor/assets/javascripts/rails.validations.js
@@ -374,7 +374,8 @@ var clientSideValidations = {
           var wrapper = element.closest('li');
           wrapper.addClass('error');
           var errorElement = $('<p class="' + settings.inline_error_class + '">' + message + '</p>');
-          wrapper.append(errorElement);
+          // wrapper.append(errorElement);
+          element.after(errorElement);
         } else {
           element.parent().find('p.' + settings.inline_error_class).text(message);
         }
